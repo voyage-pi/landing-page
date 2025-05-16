00037258d20f442e9ccf2a9f7f1dd281801c6ea0
ruimachado23 <rui.faria.machado@gmail.com>
2025-05-16 11:58:42 +0100

    invert forms colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -406,28 +406,12 @@ export default function Home() {
       </section>
 
       {/* Abstract Floating Elements Section */}
-      <section className="py-32 bg-gradient-to-r from-[#ffeaef] to-[#ffcbd5] relative overflow-hidden">
-        {/* Wave Background Elements */}
-        <div className="absolute inset-0 z-0 opacity-30">
-          <div className="wave wave6"></div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1440 320"
-            className="absolute top-0 left-0 w-full h-full"
-            preserveAspectRatio="none"
-          >
-            <path
-              className="wave-path"
-              fill="rgba(254, 56, 92, 0.2)"
-              d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,122.7C672,117,768,139,864,149.3C960,160,1056,160,1152,138.7C1248,117,1344,75,1392,53.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-            ></path>
-          </svg>
-        </div>
-
+      <section className="py-32 bg-white relative overflow-hidden">
+        {/* Remove wave background elements */}
         <div className="container mx-auto px-4 relative z-10">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
             {/* Left column: Text content */}
-            <div>
+            <div className="rounded-xl bg-white ">
               <h1 className="text-4xl font-extrabold mb-1 text-[#2c303b]">HAVE ANY QUESTIONS? </h1>
 
               <h3 className="text-xl mb-5 text-[#2c303b]/60">We'd love to hear from you</h3>
